b3f57e30447f2f7bd206c19295453afaa5a35f63
Sahil Thakur <mesahilthakur@gmail.com>
2023-12-20 18:37:32 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -41,12 +41,13 @@ function App() {
 
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
-    <div className="bg-[#172842] min-h-screen py-8">
-      <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+    <div className="bg-[#172842] min-h-screen py-8 rounded-md">
+      <div className="w-full max-w-2xl mx-auto shadow-2xl rounded-lg px-4 py-3 text-white bg-[#1c3150]">
         <h1 className="text-2xl font-bold text-center mb-8 mt-2">
           Manage Your Todos
         </h1>
         <div className="mb-4">{/* Todo form goes here */}<TodoForm/></div>
+        <h1 className=" my-4">My Todos</h1>
         <div className="flex flex-wrap gap-y-3">
           {/*Loop and Add TodoItem here */}
           {todos.map((todo)=>(
